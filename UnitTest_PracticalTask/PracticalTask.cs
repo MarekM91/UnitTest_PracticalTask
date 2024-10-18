314cea07ea997bb48ded6188106473dd6bb1c9b9
Michal Marek <marekmichal1991@gmail.com>
2024-10-18 17:57:54 +0200

    1st commit

---
+++ UnitTest_PracticalTask/PracticalTask.cs
@@ -0,0 +1,115 @@
+﻿using System.Runtime.ExceptionServices;
+
+namespace UnitTest_PracticalTask
+{
+    public class PracticalTask
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Pracical task");
+        }
+
+        public static int MaxConsecutiveDistinctCharacters(string word)
+        {
+            if (string.IsNullOrEmpty(word))
+            {
+                throw new ArgumentNullException(nameof(word), "Input cannot be null or empty.");
+            }
+
+            int maxLength = 1;
+            int currentLength = 1;
+
+            for (int i = 1; i < word.Length; i++)
+            {
+                if (word[i] != word[i - 1])
+                {
+                    currentLength++;
+                }
+                else
+                {
+                    maxLength = Math.Max(maxLength, currentLength);
+                    currentLength = 1;
+                }
+            }
+
+            maxLength = Math.Max(maxLength, currentLength);
+            return maxLength;
+        }
+
+        public static int MaxConsecutiveIdenticalLetters(string word)
+        {
+            if (string.IsNullOrEmpty(word))
+            {
+                throw new ArgumentNullException(nameof(word), "Input cannot be null or empty.");
+            }
+
+            int maxLength = 1;
+            int currentLength = 1;
+
+            for (int i = 1; i < word.Length; i++)
+            {
+                if (char.IsLetter(word[i]) && char.IsLetter(word[i - 1]) && word[i] == word[i - 1] && IsLatinLetter(word[i]))
+                {
+                    currentLength++;
+                    maxLength = Math.Max(maxLength, currentLength);
+                }
+                else
+                {
+                    currentLength = 1;
+                }
+            }
+            return maxLength;
+        }
+
+        private static bool IsLatinLetter(char c)
+        {
+            return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z');
+        }
+
+        public static int MaxConsecutiveIdenticalDigits(string word)
+        {
+            if (string.IsNullOrEmpty(word))
+            {
+                throw new ArgumentNullException(nameof(word), "Input cannot be null or empty.");
+            }
+
+            int maxLength = 1;
+            int currentLength = 1;
+
+            bool hasDigits = false;
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (char.IsDigit(word[i]))
+                {
+                    hasDigits = true;
+                    break;
+                }
+            }
+
+            if (!hasDigits)
+            {
+                return 0;
+            }
+
+            for (int i = 1; i < word.Length; i++)
+            {
+                if (char.IsDigit(word[i]) && char.IsDigit(word[i - 1]) && word[i] == word[i - 1])
+                {
+                    currentLength++;
+                    maxLength = Math.Max(maxLength, currentLength);
+                }
+                else if (char.IsDigit(word[i])) 
+                {
+                    currentLength = 1;
+                }
+                else
+                {
+                    currentLength = 0;
+                }
+            }
+            return maxLength;
+        }
+    }
+    
+}
---
+++ UnitTest_PracticalTaskTests/PracticalTaskTests.cs
@@ -0,0 +1,123 @@
+﻿using NUnit.Framework;
+using UnitTest_PracticalTask;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assert = NUnit.Framework.Assert;
+
+namespace UnitTest_PracticalTask.Tests
+{
+    [TestFixture()]
+    public class PracticalTaskTests
+    {
+        // MaxConsecutiveDistinctCharacters
+
+        [Test]
+        public void MaxConsecutiveDistinctCharacters_EmptyString_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => PracticalTask.MaxConsecutiveDistinctCharacters(""));
+        }
+
+        [Test]
+        public void MaxConsecutiveDistinctCharacters_SingleCharacter_ReturnsOne()
+        {
+            var actual = PracticalTask.MaxConsecutiveDistinctCharacters("a");
+            Assert.AreEqual(1, actual);
+        }
+
+        [Test]
+        public void MaxConsecutiveDistinctCharacters_TwoIdenticalLetters_ReturnsTwo()
+        {
+            var actual = PracticalTask.MaxConsecutiveDistinctCharacters("ab");
+            Assert.AreEqual(2, actual);
+        }
+
+        [Test]
+        public void MaxConsecutiveDistinctCharacters_AllDistinct_ReturnsLength()
+        {
+            var actual = PracticalTask.MaxConsecutiveDistinctCharacters("abcdef");
+            Assert.AreEqual(6, actual);
+        }
+
+        [Test]
+        public void MaxConsecutiveDistinctCharacters_SomeRepeats_ReturnsCorrectLength()
+        {
+            var actual = PracticalTask.MaxConsecutiveDistinctCharacters("abecaddlo");
+            Assert.AreEqual(6, actual);
+        }
+
+        // MaxConsecutiveIdenticalLetters
+
+        [Test]
+        public void MaxConsecutiveIdenticalLetters_EmptyString_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => PracticalTask.MaxConsecutiveIdenticalLetters(""));
+        }
+
+        [Test]
+        public void MaxConsecutiveIdenticalLetters_SingleLetter_ReturnsOne()
+        {
+            var actual = PracticalTask.MaxConsecutiveIdenticalLetters("a");
+            Assert.AreEqual(1, actual);
+        }
+
+        [Test]
+        public void MaxConsecutiveIdenticalLetters_TwoIdenticalLetters_ReturnsTwo()
+        {
+            var actual = PracticalTask.MaxConsecutiveIdenticalLetters("aa");
+            Assert.AreEqual(2, actual);
+        }
+
+        [Test]
+        public void MaxConsecutiveIdenticalLetters_NoIdenticalLetters_ReturnsOne()
+        {
+            var actual = PracticalTask.MaxConsecutiveIdenticalLetters("abcdefg");
+            Assert.AreEqual(1, actual);
+        }
+
+        [Test]
+        public void MaxConsecutiveIdenticalLetters_MultipleIdenticalGroups_ReturnsMaxGroupLength()
+        {
+            var actual = PracticalTask.MaxConsecutiveIdenticalLetters("aaabbbccc");
+            Assert.AreEqual(3, actual);
+        }
+
+        // MaxConsecutiveIdenticalDigits
+
+        [Test]
+        public void MaxConsecutiveIdenticalDigits_EmptyString_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => PracticalTask.MaxConsecutiveIdenticalDigits(""));
+        }
+
+        [Test]
+        public void MaxConsecutiveIdenticalDigits_SingleDigit_ReturnsOne()
+        {
+            var actual = PracticalTask.MaxConsecutiveIdenticalDigits("1");
+            Assert.AreEqual(1, actual);
+        }
+
+        [Test]
+        public void MaxConsecutiveIdenticalDigits_TwoIdenticalDigits_ReturnsTwo()
+        {
+            var actual = PracticalTask.MaxConsecutiveIdenticalDigits("11");
+            Assert.AreEqual(2, actual);
+        }
+
+        [Test]
+        public void MaxConsecutiveIdenticalDigits_NoDigits_ReturnsZero()
+        {
+            var actual = PracticalTask.MaxConsecutiveIdenticalDigits("abcdef");
+            Assert.AreEqual(0, actual);
+        }
+
+        [Test]
+        public void MaxConsecutiveIdenticalDigits_MultipleIdenticalDigits_ReturnsMaxGroupLength()
+        {
+            var actual = PracticalTask.MaxConsecutiveIdenticalDigits("111222");
+            Assert.AreEqual(3, actual);
+        }
+    }
+}
